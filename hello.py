0f9a2ccf59c6e155135907e791326c15b60f1d33
Eduardo Garnica <ec2-user@ip-172-31-31-117.eu-west-3.compute.internal>
2024-07-03 14:42:48 +0000

    adding initial structure

---
+++ hello.py
@@ -0,0 +1,12 @@
+def add(x, y):
+    return x + y
+
+
+# x=1 ## definir asi variables da error en el lint
+# y=2
+# print(f"This is a sum: {x}, {y}, {add(x,y)}")
+
+# bien hecho para el lint
+
+result = (1, 2)
+print(f"result sum: {result}")
